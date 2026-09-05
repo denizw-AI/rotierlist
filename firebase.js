50f9f2bf54883bf1564d28a0301c507f6b0ee2d1
Smituureal <denizzzezw@gmail.com>
2026-09-05 17:08:29 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+    getAuth,
+    setPersistence,
+    browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHHywPPpPDpOE2kRTjxLxCKzG_ZciimYA",
+    authDomain: "rotierlist.firebaseapp.com",
+    projectId: "rotierlist",
+    storageBucket: "rotierlist.firebasestorage.app",
+    messagingSenderId: "871029721096",
+    appId: "1:871029721096:web:66423c83edaf208c43a9bd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+setPersistence(auth, browserLocalPersistence)
+    .catch((error) => {
+        console.error("Persistence error:", error);
+    });
+
+export {
+    app,
+    auth,
+    db
+};
